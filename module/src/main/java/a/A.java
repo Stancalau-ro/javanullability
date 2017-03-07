7dd98408825f2257cc05bc4889181f50e793ead7
contact@stancalau.ro <Cristian Stancalau>
2017-03-07 19:09:41 +0200

    Initial commit

---
+++ module/src/main/java/a/A.java
@@ -0,0 +1,10 @@
+package a;
+
+public class A {
+
+    public void gg(String s) {
+        if (s != null) {
+            s = "";
+        }
+    }
+}
---
+++ src/test/java/ro/stancalau/springfx/gui/SecondPresentationTest.groovy
@@ -0,0 +1,7 @@
+package ro.stancalau.springfx.gui
+
+/**
+ * Created by Stanky on 3/6/2017.
+ */
+class SecondPresentationTest extends GroovyTestCase {
+}
